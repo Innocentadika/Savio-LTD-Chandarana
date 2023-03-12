61ddf1450acc09b5e7b12df1c8e5d28681bd92b2
innocentadika <innocentadika18@gmail.com>
2023-03-13 00:15:57 +0300

    adding component for the services offered

--- src/Assets/Services/Services.jsx
+++ src/Assets/Services/Services.jsx
@@ -1,25 +1,74 @@
 import React from 'react'
+import Compound from '../../Assets/images/garden.png'
+import Gardened from '../../Assets/images/gardened.png'
+import Computer from '../../Assets/images/e-commerse.png'
+
+
 import './services.css'
 
 const Services = () => {
   return (
-    <div className='courses'>
-        <h2>Our Popular Services</h2>
+    <div className='courses py-4'>
+        <h2 className='pb-4'>Our Popular Services</h2>
+        <div className='lg:flex space-x-3 space-y-2'>
+
+        <div className='sm:flex md:flex space-y-2 lg:space-x-3'>
   <div className="container courses_container">
   <article className="course">
       <div className="course_image">
-        <img src={''} alt=''/>
+        <img src={Compound} alt=''/>
       </div>
       <div className="course_info">
-        <h4>Responsive Design</h4>
+        <h4>Land Architecture and Development</h4>
 
       <p>
-       Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, saepe.
+      Our team of experienced landscape architects
+will work with clients to create a custom design that is tailored to their specific needs and preferences. We offer a range of services, including site analysis, concept development, master planning, construction documentation, and project management.
       </p>
       <a href="/" className="btn btn-primary">GET STARTED</a>
       </div>
       
     </article>
+    </div>
+
+
+    <div className="container courses_container">
+  <article className="course">
+      <div className="course_image">
+        <img src={Gardened} alt=''/>
+      </div>
+      <div className="course_info">
+        <h4>Landscaping Management</h4>
+
+      <p>
+      Our landscape maintenance services include mowing,
+edging, pruning, weed control, fertilization, and irrigation management. We also provide seasonal services such as leaf removal and holiday lighting.
+      </p>
+      <a href="/" className="btn btn-primary">GET STARTED</a>
+      </div>
+      
+    </article>
+    </div>
+
+    </div>
+
+    <div className="container courses_container">
+  <article className="course">
+      <div className="course_image">
+        <img src={Computer} alt=''/>
+      </div>
+      <div className="course_info">
+        <h4>E-commerce platform</h4>
+
+      <p>
+      Our online store offers a wide range of landscaping materials and plants for sale or lease, including mulch, soil, stone, pavers, and decorative plants. Customers can easily browse our inventory, place orders, and arrange for a delivery or pick-up. Our customers will also be able to lease out major landscaping materials to save on both storage and purchasing costs.
+      </p>
+      <a href="/" className="btn btn-primary">GET STARTED</a>
+      </div>
+      
+    </article>
+    </div>
+
     </div>
 
     </div>
--- src/components/Loarder/Loarder.jsx
+++ src/components/Loarder/Loarder.jsx
@@ -1,21 +1,51 @@
 import React, { Fragment } from 'react'
+import { Typewriter } from 'react-simple-typewriter'
 import Navbar from '../../Assets/navbar/Navbar'
 import Accounts from '../Accounts/Accounts'
 import Footer from '../Footer/Footer'
 import More from '../Work/Work.component'
 import Background from '../../Assets/images/background.png'
 import Testimonies from '../Testimonials/Testimonies'
+import Services from '../../Assets/Services/Services'
 
 
 const Loarder = () => {
   return (
     <div style={{backgroundImage: `url(${Background})`, backgroundSize: 'cover'}}>
+        
     <Fragment> 
                
          <Navbar/>
-                
-        <div>
+         <div className='justify-center text-2xl text-center items-center'>
+      <h1 className=' text-base text-yellow-500' style={{ paddingTop: '1rem', paddingBottom: '1rem', margin: 'auto 0', fontWeight: 'normal' }}>
+        {' '}
+        <span style={{ color: 'green', fontWeight: 'bold', fontFamily: 'sansf'}}>
+          <Typewriter
+            words={['Quality', 'Customer Service', 'Sustainability', 'Innovation', 'Excellence']}
+            loop={100}
+            cursor
+            cursorStyle='|'
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
+            
+          />
+        </span>
+        SAVIO LANDSCAPING VALUES
+      </h1>
+    </div>
 
+          <Services/>      
+        <div className='justify-center py-4 px-2'>
+                <p className='text-center text-xl text-green-800 font-extrabold py-3'>What We Believe In</p>
+                <span>
+At Savio Landscaping Services, we believe in the transformative power of nature. We believe that by
+enhancing outdoor spaces, we can improve the quality
+of life for individuals, families, and communities. We are
+dedicated to creating beautiful, functional, and
+sustainable outdoor environments that bring people
+closer to nature and enhance their wellbeing.
+                </span>
         </div>
         <Testimonies/>
         <More/>
