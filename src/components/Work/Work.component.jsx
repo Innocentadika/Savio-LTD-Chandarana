1e9770030b8a69d64722d3e4868048a629338d84
innocentadika <innocentadika18@gmail.com>
2023-03-13 11:55:35 +0300

    creation of the message section and adding background

--- src/components/Work/Work.component.jsx
+++ src/components/Work/Work.component.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import './work.css'
 import moreData from './moreData'
 import Bed from '../Bedding/Bed'
+import Maincover from '../../Assets/images/maincover.png'
 
 
 const More = () => {
   return (
     <>
     <Bed/>
-    <div className="startup">
+    <div style={{ backgroundImage: `url(${Maincover})`, backgroundSize: 'cover'}}  className="startup">
     <h1 className='text-green-700 text-5xl text-center font-extrabold'>Why you will love working with us</h1>
         <section className='startup2'>
       <div className='container grid5 mtop'>
